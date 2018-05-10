0e1355217b90d53786071aa1b5545b1f1f6ddd26
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-10 17:46:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,11 +18,10 @@ WIN_COMBINATIONS = [
 def won(board)
   # compare the indices of combo at a time to the board - use position_taken helper method
   # if there's a match, check that values are equal (all X's or all O's)
-  
+
   WIN_COMBINATIONS.detect |combination|
     position_taken?(board, combination[0]) && position_taken(board, combination[1]) &&
     position_taken(board, combination[2])
   end
 
 end
-  
